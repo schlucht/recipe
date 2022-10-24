84d749a62e890c1b250cb1d81ea9128bad788552
schlucht <web@jagolo.ch>
2022-10-24 14:19:42 +0000

    add formular

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,1 @@
 
-window.addEventListener('load', function() {
-    const btn = document.querySelector('button')
-    const txt = document.querySelector('.txt')
-    btn.addEventListener('click', () => {
-        txt.innerHTML = `<p class="text-primary">Hallo Lothar</p>`
-    })
-})
